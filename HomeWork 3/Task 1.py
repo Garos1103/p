dbc8ef94ba20c0e175b0ac53dc17dba4d38ced1d
Garos1103 <119961634+Garos1103@users.noreply.github.com>
2023-09-24 18:19:12 +0300

    Add files via upload

---
+++ HomeWork 3/Task 1.py
@@ -0,0 +1,7 @@
+import random
+
+
+original_list = [random.randint(0, 9) for _ in range(15)]
+print(original_list)
+unique_dublicate_list = list({item for item in original_list if original_list.count(item) > 1})
+print(unique_dublicate_list)
